156ab16fea35d7fe069755d0a68915a34ffff483
D-Pravallika <dpravallikarav@gmail.com>
2020-09-14 22:37:53 +0530

    goal 3 pro2

--- RDIP/Codes/in.js
+++ RDIP/Codes/in.js
@@ -172,3 +172,24 @@ var corpus_hin = [
      
      }
 
+ function shuffle(array) { //Function to shuffle the array to generate words in random order
+        var currentIndex = array.length, temporaryValue, randomIndex;
+      
+        // While there remain elements to shuffle
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+      }
+
+
+
